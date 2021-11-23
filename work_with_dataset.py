67b729057f47b3fa0c220f2a8e3affe57f857563
SvitlanaBaranchuk <sbaranchuk677@gmail.com@>
2021-11-23 21:29:18 +0200

    Added file with code.

---
+++ work_with_dataset.py
@@ -0,0 +1,24 @@
+# import of necessary libraries and modules
+from PIL import Image, ImageDraw
+
+
+def draw(name_file, name_image, size):
+    # create a picture
+    img = Image.new("RGB", size, "white")
+    # in order to draw a picture, create an ImageDraw object and pass it a picture
+    dr = ImageDraw.Draw(img)
+
+    # open the dataset
+    dataset = open(name_file, "r")
+
+    # display points at given coordinates
+    for coordinates in dataset:
+        dr.point((int(coordinates[3:]), int(coordinates[:3])), 0)
+
+    # show the picture
+    img.show()
+    # save the image in PNG format
+    img.save(name_image, "PNG")
+
+
+draw("DS6.txt", "result.png", (960, 540))
